1ed3de0d991261999b61122ba64b116f969eabb1
McStudent01 <mcmillcy@mail.uc.edu>
2023-05-30 14:26:54 -0500

    Completed 1rst part

--- src/JavaNumerics.java
+++ src/JavaNumerics.java
@@ -2,6 +2,13 @@ public class JavaNumerics
 {
     public static void main(String[] args)
     {
+        double burgerPrice = 3.50; // South Park!
+        int burgerOrder = 4;
+        double orderPrice = 0;
+
+        orderPrice = burgerPrice * burgerOrder;
+
+        System.out.println("The price of an order of " + burgerOrder + " burgers is " + orderPrice + " dollars!");
 
     }
 }
